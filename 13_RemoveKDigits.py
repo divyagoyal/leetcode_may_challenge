078f90d9775012ae099a4c3d4d87396f56d47fdf
Divya <divya05goyal@gmail.com>
2020-05-13 23:09:28 +0530

    Update 13_RemoveKDigits.py

--- 13_RemoveKDigits.py
+++ 13_RemoveKDigits.py
@@ -1,34 +1,11 @@
 class Solution:
-    def remove_num(self,l,k):
-        i = 0
-        len_list = len(l)
-  
-        if k == 0:
-            return l
-        max_num = -10000
-        max_index = 0
+    def removeKdigits(num: str, k: int) -> str:
+        stack = []
+        for n in num:
+            while k and stack and stack[-1] > n:
+                stack.pop()
+                k -= 1
+            stack.append(n)
 
-        while i < len_list:
-            if l[i] >= max_num:
-                max_num = l[i]
-                max_index = i
-                i += 1
-            else:
-                break
-        del l[max_index]
-        self.remove_num(l, k - 1)
-        
-    def removeKdigits(self, num: str, k: int) -> str:
-        l = list()
-        for i in num:
-            l.append(int(i))
-        if len(l)==k:
-            return "0"
-        else:
-            a= self.remove_num(l,k)
-            s = [str(i) for i in l]
-            return str(int("".join(s)))
-
-
-
-        
+        stack = stack[:-k] if k else stack
+        return "".join(stack).lstrip('0') or "0"
